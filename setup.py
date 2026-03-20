7676a73e74672d465db346b13209bf954e695651
Ayush <ayushbhujade2005@gmail.com>
2026-03-20 23:00:02 +0530

    folder structure ready

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup,find_packages
+
+setup(author="ayush bhujade",
+      author_email="ayushbhujade2005@gmail.com",
+      description="this project is prevent the credit card fraud detection",
+      packages=find_packages(),
+      name="src")
---
+++ utils/db_connection.py
@@ -0,0 +1,30 @@
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import pandas as pd
+from src.logger import logging
+uri = "mongodb+srv://ayushbhujade2005:ayush2005@cluster0.ymnqumu.mongodb.net/?appName=Cluster0"
+    
+class Mongoclient:
+    def __init__(self):
+        # Create a new client and connect to the server
+        self.client = MongoClient(uri, server_api=ServerApi('1'))
+
+        # Send a ping to confirm a successful connection
+        try:
+            self.client.admin.command('ping')
+            logging.info("Pinged your deployment. You successfully connected to MongoDB!")
+        except Exception as e:
+            print(e)
+    
+    def store_data(self,path):
+        df = pd.read_csv("data\creditcard.csv")
+        data=df.to_dict(orient="records")
+        db = self.client["fraud-detection"]
+        collection = db["raw_data"]
+        collection.insert_many(data)  
+        logging.info("data is store seccessfully")      
+            
+            
+clint=Mongoclient()     
+clint.store_data("data\creditcard.csv")       
